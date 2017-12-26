5a8c7316af0f19f4ac230f1b2788746b3568d9fe
Eric Marrone <emarrone17@gmail.com>
2017-12-26 15:22:49 -0500

    finish exercise

---
+++ exercise.js
@@ -0,0 +1,5 @@
+var items = ["purchase domain", "install SSL certificate", "deploy site", "celebrate success"];
+console.log("Todo:");
+for(var i = 0; i < items.length; i++){
+  console.log((i+1) + ": " + items[i]);
+}
